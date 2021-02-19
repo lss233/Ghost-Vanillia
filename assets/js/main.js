0580390a4d77de08a924bff92fa629dcda0df05c
Lss233 <8984680+lss233@users.noreply.github.com>
2021-02-19 14:20:15 +0800

    feat: lazy load feature image, avatar and icons

--- assets/js/main.js
+++ assets/js/main.js
@@ -157,14 +157,26 @@ let init = () => {
 
 };
 document.addEventListener('DOMContentLoaded', () => {
+    let replaceSrc = (s) => {
+        if(window.CDN_URL === undefined) return s;
+        if(s.startsWith('/')) {
+            return window.CDN_URL + s;
+        } else {
+            return s.replace(window.location.origin, window.CDN_URL)
+        }
+    }
     document.querySelectorAll('img').forEach(el => {
-        el.src = el.src.replace(window.location.origin, window.CDN_URL ? window.CDN_URL : '/')
+        el.src = replaceSrc(el.src)
             .replace('https://images.unsplash.com', 'https://res.cloudinary.com/dunikemu8/image/upload/unsplash');
         let srcset = el.getAttribute('srcset');
-        el.setAttribute('loading', 'lazy')
         if (srcset) {
-            el.setAttribute('srcset', srcset.replace(window.location.origin, CDN_URL))
+            el.setAttribute('srcset', replaceSrc(srcset).replaceAll(window.location.origin, CDN_URL))
         }
+        let dataSrc = el.getAttribute('data-src');
+        if(dataSrc) {
+            el.src = replaceSrc(dataSrc)
+        }
+        el.setAttribute('loading', 'lazy')
     })
 });
 if (document.readyState === 'loading') {
