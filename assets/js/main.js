387603f775151e50fdba1c1b4b2b04a50d64988a
Lss233 <8984680+lss233@users.noreply.github.com>
2021-02-19 17:18:04 +0800

    fix: inline critical js

--- assets/js/main.js
+++ assets/js/main.js
@@ -156,29 +156,6 @@ let init = () => {
     }
 
 };
-document.addEventListener('DOMContentLoaded', () => {
-    let replaceSrc = (s) => {
-        if(window.CDN_URL === undefined) return s;
-        if(s.startsWith('/')) {
-            return window.CDN_URL + s;
-        } else {
-            return s.replace(window.location.origin, window.CDN_URL)
-        }
-    }
-    document.querySelectorAll('img').forEach(el => {
-        el.src = replaceSrc(el.src)
-            .replace('https://images.unsplash.com', 'https://res.cloudinary.com/dunikemu8/image/upload/unsplash');
-        let srcset = el.getAttribute('srcset');
-        if (srcset) {
-            el.setAttribute('srcset', replaceSrc(srcset).replaceAll(window.location.origin, CDN_URL))
-        }
-        let dataSrc = el.getAttribute('data-src');
-        if(dataSrc) {
-            el.src = replaceSrc(dataSrc)
-        }
-        el.setAttribute('loading', 'lazy')
-    })
-});
 if (document.readyState === 'loading') {
     window.addEventListener('DOMContentLoaded', init);
 } else {
