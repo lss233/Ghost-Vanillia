852fdd0ae17b9b213d8aed363c915a29399fd546
Lss233 <8984680+lss233@users.noreply.github.com>
2021-02-20 16:33:55 +0800

    fix: background color

--- assets/js/background.inline.js
+++ assets/js/background.inline.js
@@ -1,5 +1,11 @@
 (function () {
     let cover_image = window._sys.cover_image;
+    if (window.CDN_URL) {
+        cover_image = window.CDN_URL + cover_image;
+        if (document.body.style.backdropFilter === undefined) {
+            cover_image = cover_image.replace('upload/', '/upload/e_saturation:80,e_blur:800/')
+        }
+    }
     function isCached(src) {
         var img = new Image();
         img.src = src;
@@ -9,17 +15,11 @@
     }
     function loadImage() {
         var filter = window._sys.colorMode == 'dark' ? 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),' : 'linear-gradient(rgba(0, 0, 0, 0.05), rgba(0, 0, 0, 0.05)),';
-        if (window.CDN_URL) {
-            cover_image = window.CDN_URL + cover_image;
-            if (document.body.style.backdropFilter === undefined) {
-                cover_image = cover_image.replace('upload/', '/upload/e_saturation:80,e_blur:800/')
-            }
-        }
         document.getElementsByTagName('body')[0].classList.add('blur');
         document.getElementsByTagName('body')[0].style.backgroundSize = 'cover';
         document.getElementsByTagName('body')[0].style.backgroundImage = filter + `url(${cover_image})`;
     }
-    window._sys.loadBackground = loadImage;
+    window._sys.loadBackgroundImage = loadImage;
     if (isCached(cover_image)) {
         loadImage();
     } else {
