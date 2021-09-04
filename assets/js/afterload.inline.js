e90367359c9cda562e2bec5c2706b2333c0dd81c
Lss233 <8984680+lss233@users.noreply.github.com>
2021-09-04 22:44:58 +0800

    fix: hightlight.js won't load

--- assets/js/afterload.inline.js
+++ assets/js/afterload.inline.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
     function loadContent() {
         let style = document.createElement('style');
         document.head.appendChild(style);
@@ -8,26 +8,26 @@
     window._forceLoad = loadContent
     window.addEventListener('load', () => setTimeout(loadContent, 10))
     window.addEventListener('load', () => {
-        if(!window._sys.postloadScript) {
-            window._sys.postloadScript = [
-                ['//cdn.jsdelivr.net/gh/highlightjs/cdn-release@10.1.2/build/highlight.min.js', () => {
-                    document.querySelectorAll('pre code').forEach((block) => {
-                        hljs.highlightBlock(block);
-                    });
-                    hljs.initHighlightingOnLoad();
-                }]
-            ]
-        }
-        if(window.disqus_domain) {
+        window._sys.postloadScript = window._sys.postloadScript || [];
+        window._sys.postloadScript.push(['//cdn.jsdelivr.net/gh/highlightjs/cdn-release@10.1.2/build/highlight.min.js', () => {
+            document.querySelectorAll('pre code').forEach((block) => {
+                hljs.highlightBlock(block);
+            });
+            hljs.initHighlightingOnLoad();
+        }])
+        if (window.disqus_domain) {
             window._sys.postloadScript.push([`//${disqus_domain}/count.js`, null])
         }
-        
-        !function (e, t, r) { function n() { for (; d[0] && "loaded" == d[0][f];)c = d.shift(), c[o] = !i.parentNode.insertBefore(c, i) } for (var s, a, c, d = [], i = e.scripts[0], o = "onreadystatechange", f = "readyState"; s = r.shift();)a = e.createElement(t), "async" in i ? (a.async = !1, e.head.appendChild(a)) : i[f] ? (d.push(a), a[o] = n) : e.write("<" + t + ' src="' + s[0] + '" defer></' + t + ">"), a.src = s[0], a.onload = s[1] }
+
+        ! function(e, t, r) {
+            function n() { for (; d[0] && "loaded" == d[0][f];) c = d.shift(), c[o] = !i.parentNode.insertBefore(c, i) }
+            for (var s, a, c, d = [], i = e.scripts[0], o = "onreadystatechange", f = "readyState"; s = r.shift();) a = e.createElement(t), "async" in i ? (a.async = !1, e.head.appendChild(a)) : i[f] ? (d.push(a), a[o] = n) : e.write("<" + t + ' src="' + s[0] + '" defer></' + t + ">"), a.src = s[0], a.onload = s[1]
+        }
         (document, "script", window._sys.postloadScript)
     });
 })();
 // Code from https://github.com/jjranalli/nightwind/blob/main/helper.js
-(function () {
+(function() {
     function getInitialColorMode() {
         const persistedColorPreference = window.localStorage.getItem('nightwind-mode');
         const hasPersistedPreference = typeof persistedColorPreference === 'string';
@@ -41,7 +41,8 @@
         }
         return 'light';
     }
-    function toggleColorMode () {
+
+    function toggleColorMode() {
         if (!document.documentElement.classList.contains('dark')) {
             document.documentElement.classList.add('dark');
             window.localStorage.setItem('nightwind-mode', 'dark');
@@ -50,11 +51,11 @@
             window.localStorage.setItem('nightwind-mode', 'light');
         }
         window._sys.colorMode = getInitialColorMode()
-        if(window._sys.loadBackgroundImage) {
+        if (window._sys.loadBackgroundImage) {
             window._sys.loadBackgroundImage();
         }
     }
     window._sys.toggleColorMode = toggleColorMode;
-    (window._sys.colorMode = getInitialColorMode()) == 'light' ? document.documentElement.classList.remove('dark') : document.documentElement.classList.add('dark');
+    (window._sys.colorMode = getInitialColorMode()) == 'light' ? document.documentElement.classList.remove('dark'): document.documentElement.classList.add('dark');
     document.documentElement.classList.add('nightwind');
 })()
